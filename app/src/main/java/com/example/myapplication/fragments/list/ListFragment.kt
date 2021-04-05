82d0b0877ad0ce41aff38ce6e0393c02cbc21ae1
Richard <richard.vong192@gmail.com>
2021-04-05 22:48:02 +1200

    Change some stuff from the layout xml files

--- app/src/main/java/com/example/myapplication/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/list/ListFragment.kt
@@ -4,6 +4,7 @@ import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.*
+import android.widget.ListView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
